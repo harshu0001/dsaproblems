9a0d9784e294e4e10fc13f2bb264b005c2cdddb0
harshu0001 <prataph229@gmail.com>
2026-05-22 12:27:24 +0530

    hashmethods

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -8,14 +8,30 @@ Input: nums = [1, 2, 3, 3]
 Output: true
 
 '''
-def hasDuplicates(nums):
-    if(len(set(nums)) == len(nums)):
-        return False
-    else:
-        return True
+#Brute force approch
+
+# def hasDuplicates(nums):
+#     if(len(set(nums)) == len(nums)):
+#         return False
+#     else:
+#         return True
     
 
-nums = [1,2,3,3]
-print(hasDuplicates(nums))
+# nums = [1,2,3,3]
+# print(hasDuplicates(nums))
+
+## --------HASHMAP or HASHSET METHOD----------------
+
+nums = [1,2,3,4]
+
+hashset = set()
+
+for i in nums:
+    if i in hashset:
+        print("True")
+    hashset.add(i)
+
+if len(hashset) == len(nums):
+    print("No duplicates")
 
     
--- validAnagram.py
+++ validAnagram.py
@@ -14,19 +14,43 @@ Input: s = "racecar", t = "carrace"
 
 Output: true
 '''
+
+#-----BRUTE-FORCE METHOD------
+# s = "racecar"
+# t = "carrace"
+
+# out = []
+# t_list = list(t)
+# for i in s:
+#     if i in t_list:
+#         out.append("true")
+#         t_list.remove(i)
+#     else:
+#         out.append("false")
+# if "false" in out or len(s) != len(t):
+#     print("False")
+# else:
+#     print("True")
+    
+#-----HASHMAP METHOD------
+
 s = "racecar"
 t = "carrace"
 
-out = []
-t_list = list(t)
-for i in s:
-    if i in t_list:
-        out.append("true")
-        t_list.remove(i)
-    else:
-        out.append("false")
-if "false" in out or len(s) != len(t):
+map_s = {}
+map_t = {}
+
+if len(s) != len(t):
     print("False")
-else:
-    print("True")
-    
+
+for char in s:
+    map_s[char] = map_s.get(char, 0)
+
+for char in t:
+    map_t[char] = map_t.get(char, 0)
+
+for key in map_s:
+    if map_s[key] != map_t.get(key, 0):
+        print("False")
+    else:
+        print("True")
